a71a011890c9c1697bde440eec5a4eff96fcb219
Ihor Fedkiv <you@example.com>
2023-07-05 13:56:00 +0200

    first push

--- main.js
+++ main.js
@@ -1,24 +1,3 @@
-// ESM syntax is supported.
-export {};
+import { chooseCategory } from "./functions";
 
-import { question, keyInSelect } from "readline-sync";
-
-const spielerNamen = [];
-
-console.log("\nWillkommen beim Wissensduell");
-
-const spielerAnzahl =
-  keyInSelect(
-    ["Ein Spieler", "Zwei Spieler", "Drei Spieler", "Vier Spieler"],
-    "Wieviele Spieler möchten mitspielen?"
-  ) + 1;
-
-for (let i = 1; i <= spielerAnzahl; i++) {
-  const spielerName = question(`Wie lautet der Name von Spieler ${i}: `);
-  spielerNamen.push(spielerName);
-}
-
-console.log(spielerNamen[0]);
-console.log(spielerNamen[1]);
-console.log(spielerNamen[2]);
-console.log(spielerNamen[3]);
+chooseCategory();
---
+++ questionList.js
@@ -0,0 +1,55 @@
+﻿export const category1 = [
+  {
+    question: "What is the capital of France?",
+    choices: ["London", "Paris", "Berlin", "Rome"],
+    correctAnswer: "Paris",
+  },
+  {
+    question: "What is the capital of London?",
+    choices: ["London", "Paris", "Berlin", "Rome"],
+    correctAnswer: "Paris",
+  },
+  {
+    question: "What is the capital of Germany?",
+    choices: ["London", "Paris", "Berlin", "Rome"],
+    correctAnswer: "Paris",
+  },
+  {
+    question: "What is the capital of Japan?",
+    choices: ["London", "Paris", "Berlin", "Rome"],
+    correctAnswer: "Paris",
+  },
+  {
+    question: "What is the capital of USA?",
+    choices: ["London", "Paris", "Berlin", "Rome"],
+    correctAnswer: "Paris",
+  },
+];
+
+export const category2 = [
+  {
+    question: "What is the capital of France?",
+    choices: ["London", "Paris", "Berlin", "Rome"],
+    correctAnswer: "Paris",
+  },
+  {
+    question: "What is the capital of London?",
+    choices: ["London", "Paris", "Berlin", "Rome"],
+    correctAnswer: "Paris",
+  },
+  {
+    question: "What is the capital of Germany?",
+    choices: ["London", "Paris", "Berlin", "Rome"],
+    correctAnswer: "Paris",
+  },
+  {
+    question: "What is the capital of Japan?",
+    choices: ["London", "Paris", "Berlin", "Rome"],
+    correctAnswer: "Paris",
+  },
+  {
+    question: "What is the capital of USA?",
+    choices: ["London", "Paris", "Berlin", "Rome"],
+    correctAnswer: "Paris",
+  },
+];
