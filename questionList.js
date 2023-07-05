03c7891104b854e7777608cff100bac70688fe2c
Alpa <makaveli1986@hotmail.de>
2023-07-05 14:02:13 +0200

    updateQuestions

--- questionList.js
+++ questionList.js
@@ -1,55 +1,206 @@
-﻿export const category1 = [
+﻿const questionsHtml = [
   {
-    question: "What is the capital of France?",
-    choices: ["London", "Paris", "Berlin", "Rome"],
-    correctAnswer: "Paris",
+    question: "Was steht für HTML ?",
+    choices: ["Hypertext Markup Language", "Hyper Text Markup Logic", "High Text Making Language"],
+    correctAnswer: "Hypertext Markup Language",
   },
   {
-    question: "What is the capital of London?",
-    choices: ["London", "Paris", "Berlin", "Rome"],
-    correctAnswer: "Paris",
+    question: "Wie erstellt man eine Überschrift erster Ordnung in HTML ?",
+    choices: ["<heading>", "<h1>", "<title>"],
+    correctAnswer: "<h1>",
   },
   {
-    question: "What is the capital of Germany?",
-    choices: ["London", "Paris", "Berlin", "Rome"],
-    correctAnswer: "Paris",
+    question: "Wie Wie fügt man einen Link in HTML ein ?",
+    choices: ["<link>", "<a>", "<href>"],
+    correctAnswer: "<a>",
   },
   {
-    question: "What is the capital of Japan?",
-    choices: ["London", "Paris", "Berlin", "Rome"],
-    correctAnswer: "Paris",
+    question: "Wie fügt man ein Bild in HTML ein ?",
+    choices: ["<img>", "<image>", "<picture>"],
+    correctAnswer: "<img>",
   },
   {
-    question: "What is the capital of USA?",
-    choices: ["London", "Paris", "Berlin", "Rome"],
-    correctAnswer: "Paris",
+    question: "Wie erstellt man eine ungeordnete Liste in HTML?",
+    choices: ["<ul>", "<ol>", "<li>"],
+    correctAnswer: "<ul>",
   },
+  {
+    question: "Wie fügt man einen Zeilenumbruch in HTML ein?",
+    choices: ["<br>", "<break>", "<nl>"],
+    correctAnswer: "<br>",
+  },
+  {
+    question: "Wie erstellt man ein Formular in HTML?", 
+    choices: ["<form>", "<input>", "<submit>"], 
+    correctAnswer: "<form>",
+  },
+  {
+    question: "Wie fügt man eine Tabelle in HTML ein?",
+    choices: ["<table>", "<tr>", "<td>"],
+    correctAnswer: "<table>",
+  },
+  {
+    question: "Wie kommentiert man Code in HTML?",
+    choices: ["<!-- comment -->", "// comment", "/* comment */"],
+    correctAnswer: "<!-- comment -->",
+  },
+  {
+    question: "Wie bindet man eine externe CSS-Datei in HTML ein?",
+    choices: ["<link>", "<style>", "<css>"],
+    correctAnswer: "<link>",
+  },
+  {
+    question: "Wie erstellt man eine geordnete Liste in HTML?",
+    choices: ["<ol>", "<ul>", "<li>"],
+    correctAnswer: "<ol>",
+  },
+  {
+    question: "Wie erstellt man eine Schaltfläche in HTML?",
+    choices: ["<button>", "<input type=button>", "<a>"],
+    correctAnswer: "<button>",
+  },
+  {
+    question: "Wie erstellt man ein Dropdown-Menü in HTML?",
+    choices: ["<select>", "<option>", "<dropdown>"],  
+    correctAnswer: "<select>",
+  },
+  {
+    question: "Wie erstellt man eine Checkbox in HTML?",
+    choices: ["<checkbox>", "<input type=checkbox>", "<check>"],
+    correctAnswer: "<input type=checkbox>",
+  },
+  {
+    question: "Wie fügt man eine horizontale Linie in HTML ein?",
+    choices: ["<hr>", "<line>", "<hline>"],
+    correctAnswer: "<hr>",
+  },
+  {
+    question: "Wie erstellt man einen Absatz in HTML?",
+    choices: ["<p>", "<para>", "<paragraph>"],
+    correctAnswer: "<p>",
+  },
+  {
+    question: "Wie erstellt man einen Abschnitt in HTML?",
+    choices: ["<section>", "<div>", "<article>"],
+    correctAnswer: "<section>",
+  },
+  {
+    question: "Wie fügt man ein Video in HTML ein?",
+    choices: ["<video>", "<media>", "<movie>"],
+    correctAnswer: "<video>",
+  },
+  {
+    question: "Wie fügt man ein Bild mit alternativem Text in HTML ein?", 
+    choices: ["<img src=bild.jpg alt=Alternativer Text>", "<image src=bild.jpg alt=Alternativer Text>", "<picture src=bild.jpg alt=Alternativer Text>"], 
+    correctAnswer: "<img src=bild.jpg alt=Alternativer Text>",
+  },
+  {
+    question: "Wie erstellt man einen Hyperlink, der in einem neuen Tab geöffnet wird?",
+    choices: ["<a href=linkziel.html target=_blank>Linktext</a>", "<a href=linkziel.html target=_self>Linktext</a>", "<a href=linkziel.html target=_parent>Linktext</a>"],
+    correctAnswer: "<a href=linkziel.html target=_blank>Linktext</a>",
+  },
+  
 ];
 
-export const category2 = [
+const questionsCss = [
   {
-    question: "What is the capital of France?",
-    choices: ["London", "Paris", "Berlin", "Rome"],
-    correctAnswer: "Paris",
+  question: "Wie ändert man die Hintergrundfarbe eines Elements in CSS?",
+  choices: ["background-color", "color", "text-color"],
+  correctAnswer: "background-color",
   },
   {
-    question: "What is the capital of London?",
-    choices: ["London", "Paris", "Berlin", "Rome"],
-    correctAnswer: "Paris",
+  question: "Wie ändert man die Schriftgröße eines Elements in CSS?",
+  choices: ["font-size", "text-size", "size"],
+  correctAnswer: "font-size",
   },
   {
-    question: "What is the capital of Germany?",
-    choices: ["London", "Paris", "Berlin", "Rome"],
-    correctAnswer: "Paris",
+  question: "Wie zentriert man ein Element horizontal in CSS?",
+  choices: ["margin: 0 auto", "text-align: center", "align: center"],
+  correctAnswer: "margin: 0 auto",
   },
   {
-    question: "What is the capital of Japan?",
-    choices: ["London", "Paris", "Berlin", "Rome"],
-    correctAnswer: "Paris",
+  question: "Wie fügt man einen Rahmen um ein Element in CSS hinzu?",
+  choices: ["border", "outline", "frame"],
+  correctAnswer: "border",
   },
   {
-    question: "What is the capital of USA?",
-    choices: ["London", "Paris", "Berlin", "Rome"],
-    correctAnswer: "Paris",
+  question: "Wie fügt man einen Schatten zu einem Element in CSS hinzu?",
+  choices: ["box-shadow", "shadow", "element-shadow"],
+  correctAnswer: "box-shadow",
   },
-];
+  {
+  question: "Wie ändert man die Ausrichtung des Textes in CSS?",
+  choices: ["text-align", "align", "text-justify"],
+  correctAnswer: "text-align",
+  },
+  {
+  question: "Wie ändert man die Breite und Höhe eines Elements in CSS?",
+  choices: ["width und height", "size", "dimensions"],
+  correctAnswer: "width und height",
+  },
+  {
+  question: "Wie ändert man die Transparenz eines Elements in CSS?",
+  choices: ["opacity", "transparent", "visibility"],
+  correctAnswer: "opacity",
+  },
+  {
+  question: "Wie ändert man den Zeilenabstand in CSS?",
+  choices: ["line-height", "spacing", "line-spacing"],
+  correctAnswer: "line-height",
+  },
+  {
+  question: "Wie positioniert man ein Element absolut in CSS?",
+  choices: ["position: absolute", "position: fixed", "position: relative"],
+  correctAnswer: "position: absolute",
+  },
+  {
+  question: "Wie ändert man die Textfarbe eines Elements in CSS?",
+  choices: ["color", "text-color", "font-color"],
+  correctAnswer: "color",
+  },
+  {
+  question: "Wie ändert man den Abstand zwischen den Buchstaben in CSS?",
+  choices: ["letter-spacing", "spacing", "word-spacing"],
+  correctAnswer: "letter-spacing",
+  },
+  {
+  question: "Wie erstellt man eine animierte Transition in CSS?",
+  choices: ["transition", "animate", "transform"],
+  correctAnswer: "transition",
+  },
+  {
+  question: "Wie zentriert man einen Text vertikal in CSS?",
+  choices: ["line-height: height", "vertical-align: middle", "text-align: center"],
+  correctAnswer: "vertical-align: middle",
+  },
+  {
+  question: "Wie ändert man die Textdekoration in CSS?",
+  choices: ["text-decoration", "decoration", "underline"],
+  correctAnswer: "text-decoration",
+  },
+  {
+  question: "Wie ändert man den Zeichenabstand in CSS?",
+  choices: ["letter-spacing", "spacing", "word-spacing"],
+  correctAnswer: "letter-spacing",
+  },
+  {
+  question: "Wie fügt man einen Hintergrund zu einem Element in CSS hinzu?",
+  choices: ["background-image", "image", "background-url"],
+  correctAnswer: "background-image",
+  },
+  {
+  question: "Wie ändert man die Ausrichtung eines Hintergrundbildes in CSS?",
+  choices: ["background-position", "image-position", "background-align"],
+  correctAnswer: "background-position",
+  },
+  {
+  question: "Wie fügt man eine Animation zu einem Element in CSS hinzu?",
+  choices: ["@keyframes", "animation", "animate"],
+  correctAnswer: "animation",
+  },
+  {
+    question: "Wie erstellt man abgerundete Ecken an einem Element in CSS?",
+    choices: ["border-radius", "corner-radius", "rounded-corners"],
+    correctAnswer: "border-radius",
+    }
+  ];
