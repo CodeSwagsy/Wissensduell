fc10f79d874a15a174c52cf70024ca945d6c3cb8
Ihor Fedkiv <you@example.com>
2023-07-06 10:55:36 +0200

    Implementation of the code from Manuel

--- main.js
+++ main.js
@@ -1,3 +1,41 @@
-import { chooseCategory } from "./functions";
+import { question, keyInSelect } from "readline-sync";
+import {
+  chooseCategory,
+  shuffleArray,
+  createPlayer,
+  showResults,
+} from "./functions";
 
-chooseCategory();
+export const spielerArr = [];
+let spielerList = "";
+
+console.log("\nWillkommen beim Wissensduell");
+
+const spielerAnzahl =
+  keyInSelect(
+    ["Ein Spieler", "Zwei Spieler", "Drei Spieler", "Vier Spieler"],
+    "Wieviele Spieler möchten mitspielen?"
+  ) + 1;
+
+for (let i = 1; i <= spielerAnzahl; i++) {
+  let spielerName = question(`\nWie lautet der Name von Spieler ${i}: `);
+  while (!spielerName) {
+    spielerName = question(`\nWie lautet der Name von Spieler ${i}: `);
+  }
+  const playerObj = createPlayer(spielerName);
+  spielerArr.push(playerObj);
+  spielerList += spielerName + ", ";
+}
+
+console.log(
+  `\nFolgende Spieler spielen also mit: ${spielerList.slice(0, -2)}\n`
+);
+
+shuffleArray(spielerArr);
+
+spielerArr.forEach((spieler) => {
+  console.log(`${spieler.name} beginnt.`);
+  spieler.punktzahl = chooseCategory();
+});
+
+showResults();
